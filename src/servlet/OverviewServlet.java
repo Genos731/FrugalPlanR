018ec263fa77981a10afe679ab04bf12405c2423
deafgrape <notnadroj@gmail.com>
2017-10-08 22:08:55 +1100

    Update OverviewServlet.java

--- src/servlet/OverviewServlet.java
+++ src/servlet/OverviewServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class OverviewServlet
  */
-@WebServlet("/")
+@WebServlet("/overview")
 public class OverviewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
