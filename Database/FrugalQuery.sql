0f264208cbd13f941ca8a876c1825b868740d372
deafgrape <notnadroj@gmail.com>
2017-10-22 01:30:07 +1000

    updated insert query
    stores hashed password "password" for default accounts

--- Database/FrugalQuery.sql
+++ Database/FrugalQuery.sql
@@ -12,13 +12,13 @@ VALUES ("MONTHLY");
 
 
 INSERT INTO account (username, password, email)
-VALUES ("Antoine", "Antoine", "Antoine@email.com");
+VALUES ("Antoine", "1216985755", "Antoine@email.com");
 
 INSERT INTO account (username, password, email)
-VALUES ("Betsy", "Betsy", "Betsy@email.com");
+VALUES ("Betsy", "1216985755", "Betsy@email.com");
 
 INSERT INTO account (username, password, email)
-VALUES ("Cockless", "Cockless", "Cockless@email.com");
+VALUES ("Charlie", "1216985755", "Charlie@email.com");
 
 INSERT INTO category (name, account_id)
 VALUES ("Food", 1);
@@ -37,4 +37,4 @@ INSERT INTO transaction (isIncome, value, date, description, location, repeating
 VALUES (0, 329, "2015-03-22", "Rent Money", "Sydney", 1, 1, 1);
 
 INSERT INTO transaction (isIncome, value, date, description, location, repeating_id, category_id, account_id)
-VALUES (1, 69, "2016-02-02", "train money", "Melbourne", 1, 2, 2);
+VALUES (1, 69, "2016-02-02", "Train Money", "Melbourne", 1, 2, 2);
